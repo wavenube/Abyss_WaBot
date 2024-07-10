92c171fcb586ad9489edeb5a75d2be52bdd104b8
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-07-10 08:23:48 -0400

    Add files via upload

--- src/RecursosMarcoRota/Instaladores/InstalarWhisper.cppAPT.sh
+++ src/RecursosMarcoRota/Instaladores/InstalarWhisper.cppAPT.sh
@@ -1,3 +1,5 @@
 rm -rf whisper.cpp
-apt-get install git curl wget make build-essential cmake ffmpeg -y > /dev/null 2>&1 && git clone https://github.com/ggerganov/whisper.cpp.git > /dev/null 2>&1 && cd whisper.cpp && ./models/download-ggml-model.sh small > /dev/null 2>&1 && make > /dev/null 2>&1 && cd ../ && chmod +x src/RecursosMarcoRota/ScriptActivo/whispercpp.sh && cp src/RecursosMarcoRota/PluginsMovibles/txt.js plugins/txt.js
+apt-get install git curl wget make libopenblas-dev build-essential cmake ffmpeg -y > /dev/null 2>&1
+sudo update-alternatives --config libblas.so.3
+git clone https://github.com/ggerganov/whisper.cpp.git > /dev/null 2>&1 && cd whisper.cpp && ./models/download-ggml-model.sh small > /dev/null 2>&1 && GGML_OPENBLAS=1 make -j > /dev/null 2>&1 && cd ../ && chmod +x src/RecursosMarcoRota/ScriptActivo/whispercpp.sh && cp src/RecursosMarcoRota/PluginsMovibles/txt.js plugins/txt.js
 echo Si todo salio bien, deberias poder usar .txt por un audio para empezar la transcripcion. La precisin y velocidad pueden ser mayores haciendo unos cambios en el script whispercpp.sh. La documentacion en https://github.com/ggerganov/whisper.cpp
