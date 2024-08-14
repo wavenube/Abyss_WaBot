4ba33f160bad94203d9a5bf7ae02dc6db632bb7b
wavenube <wavenub3@gmail.com>
2024-08-14 14:19:57 +0100

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -142,13 +142,6 @@ if (!chat.isBanned && m.text.match(/(No estoy hablando|no estoy hablando)/gi)) {
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'no estoy hablando.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 }
 
-if (!chat.isBanned && m.text.match(/(Bañate|bañate)/gi)) {
-    if (!db.data.chats[m.chat].audios) return;
-    if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/Banate.mp3';
-    mconn.conn.sendPresenceUpdate('recording', m.chat);
-    mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'Banate.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-}
 
 if (!chat.isBanned && m.text.match(/(Silencio|silencio)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
