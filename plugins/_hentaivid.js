69fc7fc5956204f9990df6bd7def19fd15a230b9
wavenube <wavenub3@gmail.com>
2024-08-15 21:10:22 +0100

    Update and rename _prueba.js to _hentaivid.js

--- plugins/_hentaivid.js
+++ plugins/_hentaivid.js
@@ -1,6 +1,19 @@
 // Lista de URLs de videos
 const VIDEO_URLS = [
     'https://qu.ax/scZw.mp4',
+    'https://qu.ax/sKfw.mp4',
+    'https://qu.ax/TOgC.mp4',
+    'https://qu.ax/YHAm.mp4',
+    'https://qu.ax/rvnd.mp4',
+    'https://qu.ax/rvnd.mp4',
+    'https://qu.ax/OxHZ.mp4',
+    'https://qu.ax/aUXT.mp4',
+    'https://qu.ax/wbJN.mp4',
+    'https://qu.ax/fJf.mp4',
+    'https://qu.ax/bFdc.mp4',
+    'https://qu.ax/vHNs.mp4',
+    'https://qu.ax/uppk.mp4',
+    '',
     // Agrega más enlaces según sea necesario
 ];
 
@@ -32,7 +45,7 @@ const handler = async (m, { conn }) => {
 };
 
 // Definir el comando y sus propiedades
-handler.command = /^(hentaivid|video)$/i; // Comando para activar el manejador
+handler.command = /^(hentaivid|hentai)$/i; // Comando para activar el manejador
 handler.group = true; // Si el comando debe funcionar solo en grupos
 handler.admin = false; // Cambiar a true si solo administradores pueden usarlo
 handler.botAdmin = false; // Cambiar a true si el bot debe ser admin para usar el comando
