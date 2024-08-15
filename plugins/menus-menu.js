5653ffb7195619cc4c9d2c8011cf22ce3dc66026
wavenube <wavenub3@gmail.com>
2024-08-15 12:04:13 +0100

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -32,7 +32,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `Abyss - Bot
+    const str = `${tradutor.texto1[0]}
 
 ${tradutor.texto1[1]} ${taguser}
 
