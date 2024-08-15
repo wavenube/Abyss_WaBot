aafeb4cc8fd870c68cb025124b0419f683bccbc1
wavenube <wavenub3@gmail.com>
2024-08-15 19:19:49 +0100

    Update _prueba2.js

--- plugins/_prueba2.js
+++ plugins/_prueba2.js
@@ -1,38 +1,26 @@
-import { hentai } from 'hentai';
-import fs from 'fs';
-import { promisify } from 'util';
+import { Hentai, Format, Utils } from 'hentai';  // Importa las funciones necesarias
 
 const handler = async (m, { conn, usedPrefix, command }) => {
     try {
-        // Obtén un ID de doujin al azar
-        const random_id = hentai.Utils.get_random_id();
+        // Obtener un ID de doujin al azar
+        const random_id = Utils.get_random_id();
 
-        // Obtén el doujin utilizando el ID
-        const doujin = new hentai.Hentai(random_id);
+        // Crear una instancia del doujin utilizando el ID
+        const doujin = new Hentai(random_id);
 
-        // Verifica si existe
-        if (!hentai.Hentai.exists(doujin.id)) {
+        // Verificar si el doujin existe
+        if (!Hentai.exists(doujin.id)) {
             return m.reply('No se encontró ningún contenido con ese ID.');
         }
 
         // Obtener las URLs de las imágenes
         const imageUrls = doujin.image_urls;
 
-        // Enviar las imágenes al chat
-        for (let url of imageUrls) {
-            // Descargar la imagen
-            const response = await fetch(url);
-            const buffer = await response.buffer();
-
-            // Guardar la imagen temporalmente
-            const fileName = `./temp/${doujin.id}_${imageUrls.indexOf(url)}.jpg`;
-            await promisify(fs.writeFile)(fileName, buffer);
-
-            // Enviar la imagen
-            await conn.sendFile(m.chat, fileName, `${doujin.title(Format.Pretty)}.jpg`, null, m);
-
-            // Eliminar la imagen temporalmente guardada
-            await promisify(fs.unlink)(fileName);
+        // Enviar la primera imagen al chat
+        if (imageUrls.length > 0) {
+            await conn.sendFile(m.chat, imageUrls[0], `${doujin.title(Format.Pretty)}.jpg`, null, m);
+        } else {
+            return m.reply('No se encontraron imágenes en este doujin.');
         }
     } catch (error) {
         console.error(error);
@@ -40,6 +28,8 @@ const handler = async (m, { conn, usedPrefix, command }) => {
     }
 };
 
-handler.command = /^(animevid2)$/i;
+// Registro del comando
+handler.command = /^(animevid2)$/i;  // Configura el comando para que responda a "animevid"
 
+// Exporta el handler para que sea reconocido como un comando
 export default handler;
