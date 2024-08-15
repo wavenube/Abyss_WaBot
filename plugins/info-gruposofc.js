4e1de4ca7f582ca7a4edd70c9d26323bec7eea56
wavenube <wavenub3@gmail.com>
2024-08-15 11:43:13 +0100

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -11,21 +11,10 @@ const handler = async (m, {conn, usedPrefix}) => {
   const text = `${tradutor.texto1[0]}
 
 ${tradutor.texto1[1]}
-1.- https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj
-
-2.- https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
-
-3.- https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
-
-4.- https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-> MoonLight Team OFC :
-
-1.- https://whatsapp.com/channel/0029Vablj8M05MUnustq3q1S
-
-2.- https://chat.whatsapp.com/GpbF3JCTEr2CSj3zLJ1IQj`.trim();
+1.- https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6
+`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6`},
     'mimetype': `application/${document}`,
     'fileName': `${tradutor.texto2}`,
     'fileLength': 99999999999999,
@@ -34,13 +23,13 @@ ${tradutor.texto1[1]}
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': `${tradutor.texto3}`,
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': 'https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
