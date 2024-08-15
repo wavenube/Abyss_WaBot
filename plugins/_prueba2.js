5a522911c82cb10d1ddfe60cf8ca86bf60662719
wavenube <wavenub3@gmail.com>
2024-08-15 19:41:41 +0100

    Update _prueba2.js

--- plugins/_prueba2.js
+++ plugins/_prueba2.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const handler = async (m, { conn }) => {
-    const videosDir = path.join(__dirname, 'src', 'videos');
+    const videosDir = path.join(__dirname, './src', 'videos');
 
     try {
         // Leer los archivos en la carpeta de videos
