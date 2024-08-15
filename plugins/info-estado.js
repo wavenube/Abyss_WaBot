59e4f2cdb3195cdbb0b70117fe7ae4ad61bf42f7
wavenube <wavenub3@gmail.com>
2024-08-15 11:49:04 +0100

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -31,9 +31,9 @@ const handler = async (m, { conn, usedPrefix }) => {
   const wm = 'The Mystic Bot';
   const info = ` ${tradutor.texto1[0]}
 
-  ${tradutor.texto1[1]} Bruno Sobrino
-  ${tradutor.texto1[2]} +5219992095479
-  ${tradutor.texto1[3]} paypal.me/TheShadowBrokers133
+  ${tradutor.texto1[1]} ZephyrByte
+  ${tradutor.texto1[2]} +5492613619545
+  ${tradutor.texto1[3]} 
 
   ${tradutor.texto1[4]} ${rtime}
   ${tradutor.texto1[5]} ${uptime}
@@ -63,7 +63,7 @@ const handler = async (m, { conn, usedPrefix }) => {
   ];
   const document = doc[Math.floor(Math.random() * doc.length)];
   const Message = {
-    document: { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
+    document: { url: `https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6` },
     mimetype: `application/${document}`,
     fileName: `Documento`,
     fileLength: 99999999999999,
@@ -72,13 +72,13 @@ const handler = async (m, { conn, usedPrefix }) => {
       forwardingScore: 200,
       isForwarded: true,
       externalAdReply: {
-        mediaUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+        mediaUrl: "https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6",
         mediaType: 2,
         previewType: "pdf",
         title: "The Mystic - Bot",
         body: tradutor.texto2,
         thumbnail: imagen1,
-        sourceUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
+        sourceUrl: "https://chat.whatsapp.com/IYsbScC3cMzBcakINRWJL6",
       },
     },
     caption: info,
