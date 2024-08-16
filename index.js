d9ae9d54d65e8e2bd4ef3a92571fa0d4d0b21ed2
wavenube <wavenub3@gmail.com>
2024-08-16 14:51:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('Abyss\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`beta`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
