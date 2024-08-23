7b51584e47e6fa0f86bcf1510d8d490b5a344092
wavenube <wavenub3@gmail.com>
2024-08-23 18:14:29 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1634,7 +1634,7 @@ export async function participantsUpdate({ id, participants, action }) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m?.conn?.groupMetadata(id) || (conn?.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = 'https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/avatar_contact.png';
+          let pp = './src/abyss5.png';
           try {
             pp = await m.conn.profilePictureUrl(user, 'image');
           } catch (e) {
