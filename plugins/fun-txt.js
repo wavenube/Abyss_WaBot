81b5d740fc734e5ed50cded992e5c7e6c688811f
wavenube <wavenub3@gmail.com>
2024-08-25 13:04:02 +0100

    Update fun-txt.js

--- plugins/fun-txt.js
+++ plugins/fun-txt.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
      if (!teks) throw `📝 Que escribo? Ejemplo : *${usedPrefix + command}* Hola puercos`
-      m.react(rwait)
+      conn.sendMessage(m.chat, { react: { text: "🗣️", key: m.key }})
       let img = global.API('fgmods', '/api/maker/txt', { text: teks }, 'apikey')
       conn.sendFile(m.chat, img, 'img.png', `✅ Es mejor de lo que escribes tú ✍🏻`, m)
-      m.react(done)
+      conn.sendMessage(m.chat, { react: { text: "🗣️", key: m.key }})
 
   }
   handler.help = ['txt']
