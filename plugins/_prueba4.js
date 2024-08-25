54f6e60f437b408140c5c59e4fc0a0d82cd78145
wavenube <wavenub3@gmail.com>
2024-08-26 00:49:33 +0100

    Update _prueba4.js

--- plugins/_prueba4.js
+++ plugins/_prueba4.js
@@ -1,3 +1,62 @@
+let lastDecoratedMessage = null; // Declaración global
+
+// Comando 'decorar'
+const handlerDecorate = async (m, { conn, text }) => {
+    if (!text) throw '⚠️ *Por favor, escribe el texto que quieres decorar.*';
+
+    const str = `${text}`.trim();
+    const pp = 'https://i.ibb.co/Qjf1sdk/abyss-profile.png'; // Puedes cambiar la URL de la imagen
+
+    const fkontak = {
+        key: { 
+            participants: "0@s.whatsapp.net", 
+            remoteJid: "status@broadcast", 
+            fromMe: false, 
+            id: "Halo" 
+        }, 
+        message: { 
+            contactMessage: { 
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        participant: "0@s.whatsapp.net" 
+    };
+
+    const messageOptions = {
+        image: { url: pp },
+        caption: str,
+        mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net'),
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: "120363318622514917@newsletter",
+                serverMessageId: 1,
+                newsletterName: "<Abyss - Bot>",
+            },
+            externalAdReply: {
+                mediaUrl: "https://whatsapp.com/channel/0029VakDx9I0gcfFXnzZIX2v",
+                mediaType: 'VIDEO',
+                description: 'canal del grupo',
+                title: 'wm',
+                body: "By: ZephyrByte",
+                thumbnailUrl: "https://i.ibb.co/Qjf1sdk/abyss-profile.png",
+                sourceUrl: "https://whatsapp.com/channel/0029VakDx9I0gcfFXnzZIX2v"
+            }
+        }
+    };
+
+    // Enviar el mensaje decorado
+    const sentMsg = await conn.sendMessage(m.chat, messageOptions, { quoted: fkontak });
+
+    // Guardar el último mensaje decorado
+    lastDecoratedMessage = sentMsg;
+};
+
+handlerDecorate.command = /^(decorar2)$/i;
+handlerDecorate.exp = 50;
+export default handlerDecorate;
+
+// Comando 'enviar'
 const handlerEnviar = async (m, { conn }) => {
     if (!lastDecoratedMessage) throw '⚠️ *No hay ningún mensaje decorado para reenviar.*';
 
@@ -17,5 +76,4 @@ const handlerEnviar = async (m, { conn }) => {
 
 handlerEnviar.command = /^(enviar)$/i;
 handlerEnviar.exp = 50;
-
 export default handlerEnviar;
