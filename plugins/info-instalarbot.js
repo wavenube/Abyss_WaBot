b56b53f3a678f1d4c4e128e0578252f6d195e42c
wavenube <wavenub3@gmail.com>
2024-08-27 14:20:44 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,7 +1,7 @@
 const handlerInstallBot = async (m, { conn }) => {
-    const installMessage = `
+    const str = `
 ╭─────────────┈⊷
-│ \`COMO INSTALAR ESTE BOT?\`
+│ COMO INSTALAR ESTE BOT?
 ╰┬────────────┈⊷
 ┌┤📌 *REQUISITOS PARA LA INSTALACION*
 ┌┤❇️ _Dudas: wa.me/5492613619545_
@@ -17,10 +17,52 @@ const handlerInstallBot = async (m, { conn }) => {
 
 > (𝘌𝘴𝘤𝘢𝘯𝘦𝘢𝘳 𝘦𝘭 𝘘𝘙 o código, 𝘙𝘢𝘱𝘪𝘥𝘰)
 
+🚀 --------[ Cafirexos host ]--------- 🚀
+
+*Página oficial:*
+https://www.cafirexos.com/
+
+*Panel:*
+https://panel.cafirexos.com/
+
+*Dashboard:*
+https://dash.cafirexos.com/home
+
+*Dudas UNICAMENTE SOBRE EL HOST:*
+https://chat.whatsapp.com/FBtyc8Q5w2iJXVl5zGJdFJ 
+_(Pregunta por Diego: cafirexos)_
+
+*Canal de WhatsApp:*
+https://whatsapp.com/channel/0029VaFVSkRCMY0KFmCMDX2q
     `;
 
-    // Enviar el mensaje con las instrucciones de instalación
-    await conn.sendMessage(m.chat, { text: installMessage }, { quoted: m });
+    const pp = './src/abyss.png'; // Ruta o URL de la imagen
+
+    const messageOptions = {
+        image: { url: pp },
+        caption: str,
+        mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net'),
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: "120363318622514917@newsletter",
+                serverMessageId: 1,
+                newsletterName: "<Abyss - Bot>",
+            },
+            externalAdReply: {
+                mediaUrl: "https://whatsapp.com/channel/0029VakDx9I0gcfFXnzZIX2v",
+                mediaType: 'VIDEO',
+                description: 'canal del grupo',
+                title: 'wm', // Aquí puedes reemplazar con el texto que desees
+                body: "By: ZephyrByte",
+                thumbnailUrl: "https://i.ibb.co/Qjf1sdk/abyss-profile.png", // Puedes cambiar la URL de la miniatura
+                sourceUrl: "https://whatsapp.com/channel/0029VakDx9I0gcfFXnzZIX2v"
+            }
+        }
+    };
+
+    // Enviar el mensaje decorado con las instrucciones de instalación
+    await conn.sendMessage(m.chat, messageOptions, { quoted: m });
 };
 
 // Configuración del comando
