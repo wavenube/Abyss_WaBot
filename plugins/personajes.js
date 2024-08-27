5efbc8503032ea947cc08316658b7d4f5f0569ee
wavenube <wavenub3@gmail.com>
2024-08-27 19:04:41 +0100

    Update personajes.js

--- plugins/personajes.js
+++ plugins/personajes.js
@@ -6,6 +6,7 @@ export const personajes = [
         imagen: "https://th.bing.com/th/id/R.fa347836243872f38cee39a3025667a2?rik=7RDa%2bhrvRpqfLQ&riu=http%3a%2f%2fimages2.fanpop.com%2fimage%2fphotos%2f10900000%2fUzumaki-Naruto-naruto-10920951-1208-935.jpg&ehk=glfjbJV7tgePSw70K36dtpafd3wqfo%2fzrGtnDqplsrw%3d&risl=&pid=ImgRaw&r=0",
         titulo: "El Séptimo Hokage",
         descripcion: "Un ninja con un gran corazón y una determinación inquebrantable.",
+        valor: 5,  // Valor en diamantes
         estado: "libre"  // Estado inicial
     },
     {
@@ -13,6 +14,7 @@ export const personajes = [
         imagen: "https://th.bing.com/th/id/R.412d5344e572900055bfbd608acb0731?rik=nQcBTurtsA62CA&riu=http%3a%2f%2fwallsdesk.com%2fwp-content%2fuploads%2f2016%2f12%2fSasuke-Uchiha-full-HD.jpg&ehk=K%2fb2fNix9GjM6OXa3LKArJnFl%2fRBgZ4go9UkODGX6RQ%3d&risl=&pid=ImgRaw&r=0",
         titulo: "El Último Uchiha",
         descripcion: "Un prodigio del clan Uchiha con un pasado sombrío.",
+        valor: 5,  // Valor en diamantes
         estado: "libre"  // Estado inicial
     },
     {
@@ -20,6 +22,7 @@ export const personajes = [
         imagen: "https://th.bing.com/th/id/R.522e40edeac3d06ef3c7449ad93166c4?rik=C1FW%2bJ1O%2fb4v1g&riu=http%3a%2f%2fnarutorealm.files.wordpress.com%2f2011%2f11%2fhinata_hyuuga__chapter_437_by_explosivechemicals.png&ehk=Fa3sS0nJkz%2bVrUgVOYX80WlaGmr%2bE6Hm1ddQOhKqYCY%3d&risl=&pid=ImgRaw&r=0",
         titulo: "La blanquita tetona",
         descripcion: "nose que decirte, solo vi su rule",
+        "valor": "5",  // Valor en diamantes
         estado: "libre"  // Estado inicial
     },
     {
@@ -27,6 +30,7 @@ export const personajes = [
         "imagen": "./src/fotos/Goku-4K-HD-Wallpaper-Free-download.jpg",
         "titulo": "El Saiyajin Legendario",
         "descripcion": "Un guerrero de corazón puro y la fuerza de un dios.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -34,6 +38,7 @@ export const personajes = [
         "imagen": "https://rare-gallery.com/mocahbig/1375182-luffy-gear-5-vs-kaido-dragon-one-piece-4k-pc.jpg",
         "titulo": "El Capitán de los Sombrero de Paja",
         "descripcion": "Un aventurero que busca el One Piece y el título de Rey de los Piratas.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -41,6 +46,7 @@ export const personajes = [
         "imagen": "https://images.wallpapersden.com/image/download/spike-spiegel-cowboy-bebop-4k-art_a25mZ2WUmZqaraWkpJRnamtlrWZpaWU.jpg",
         "titulo": "El Cazador de Recompensas",
         "descripcion": "Un cazarrecompensas con un pasado oscuro y un estilo de vida relajado.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -48,6 +54,7 @@ export const personajes = [
         "imagen": "https://th.bing.com/th/id/OIP.79eFKkeycKJQM0x6rlwm3AHaEo?rs=1&pid=ImgDetMain",
         "titulo": "La Ninja Médica",
         "descripcion": "Una kunoichi que combina habilidades médicas con un gran poder de combate.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -55,6 +62,7 @@ export const personajes = [
         "imagen": "https://wallpapercave.com/wp/wp1975719.png",
         "titulo": "La Guerrera Imparable",
         "descripcion": "La soldado más fuerte del Cuerpo de Exploración, conocida por su habilidad en combate y lealtad.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -62,6 +70,7 @@ export const personajes = [
         "imagen": "https://th.bing.com/th/id/R.3fe0270b01778ca48adc11ff6c28ff86?rik=Y2viXirUXqrx4w&pid=ImgRaw&r=0",
         "titulo": "El Alquimista de Acero",
         "descripcion": "Un joven alquimista con un gran talento y un fuerte deseo de restaurar su cuerpo perdido.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -69,6 +78,7 @@ export const personajes = [
         "imagen": "https://wallpaperaccess.com/full/1977407.png",
         "titulo": "La Piloto de EVA",
         "descripcion": "Una joven piloto de EVA con una personalidad fuerte y habilidades excepcionales en combate.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -76,6 +86,7 @@ export const personajes = [
         "imagen": "https://th.bing.com/th/id/R.af80b26c6bd5a71eba219eb1e10b579b?rik=sPxSusXKl81zDg&pid=ImgRaw&r=0",
         "titulo": "El Híbrido de Demonio y Humano",
         "descripcion": "Un medio-demonio con una personalidad feroz en busca de la perla de Shikon.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -83,6 +94,7 @@ export const personajes = [
         "imagen": "https://th.bing.com/th/id/R.dcd8ee87895b6a350efcc357a5cf1758?rik=xxowEpoYfol%2bDQ&pid=ImgRaw&r=0",
         "titulo": "El Dragón de Fuego",
         "descripcion": "Un mago de fuego con una personalidad ardiente y un fuerte sentido de la justicia.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     },
     {
@@ -90,6 +102,7 @@ export const personajes = [
         "imagen": "https://getwallpapers.com/wallpaper/full/2/e/0/1386406-light-yagami-wallpaper-1920x1080-for-iphone-5s.jpg",
         "titulo": "El Rey del Mundo",
         "descripcion": "Un estudiante brillante que encuentra una libreta con el poder de decidir la vida o muerte de cualquier persona.",
+        "valor": "5",  // Valor en diamantes
         "estado": "libre"
     }
     // Agrega más personajes aquí
