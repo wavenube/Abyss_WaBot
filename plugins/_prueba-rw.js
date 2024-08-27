6d0a6e22b1fef7344d96043a1023b95306075d24
wavenube <wavenub3@gmail.com>
2024-08-27 16:33:09 +0100

    Update _prueba-rw.js

--- plugins/_prueba-rw.js
+++ plugins/_prueba-rw.js
@@ -3,13 +3,13 @@ import { createHash } from 'crypto';
 const personajes = [
     {
         nombre: "Naruto Uzumaki",
-        imagen: "https://example.com/naruto.jpg",
+        imagen: "https://th.bing.com/th/id/R.d536e2ce81f57260a1b086b8eb72cfed?rik=M%2b4DWVkBbtGdHA&pid=ImgRaw&r=0",
         titulo: "El Séptimo Hokage",
         descripcion: "Un ninja con un gran corazón y una determinación inquebrantable."
     },
     {
         nombre: "Sasuke Uchiha",
-        imagen: "https://example.com/sasuke.jpg",
+        imagen: "https://th.bing.com/th/id/OIP.yY8XGyS5FU5VyJuc-4eDaAHaFj?rs=1&pid=ImgDetMain",
         titulo: "El Último Uchiha",
         descripcion: "Un prodigio del clan Uchiha con un pasado sombrío."
     },
