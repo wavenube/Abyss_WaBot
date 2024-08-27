d126efa9216027b0b7e1615326edcaab9b9ae178
wavenube <wavenub3@gmail.com>
2024-08-27 17:47:29 +0100

    Update personajes.js

--- plugins/personajes.js
+++ plugins/personajes.js
@@ -3,17 +3,94 @@
 export const personajes = [
     {
         nombre: "Naruto Uzumaki",
-        imagen: "https://th.bing.com/th/id/R.d536e2ce81f57260a1b086b8eb72cfed?rik=M%2b4DWVkBbtGdHA&pid=ImgRaw&r=0",
+        imagen: "https://th.bing.com/th/id/R.fa347836243872f38cee39a3025667a2?rik=7RDa%2bhrvRpqfLQ&riu=http%3a%2f%2fimages2.fanpop.com%2fimage%2fphotos%2f10900000%2fUzumaki-Naruto-naruto-10920951-1208-935.jpg&ehk=glfjbJV7tgePSw70K36dtpafd3wqfo%2fzrGtnDqplsrw%3d&risl=&pid=ImgRaw&r=0",
         titulo: "El Séptimo Hokage",
         descripcion: "Un ninja con un gran corazón y una determinación inquebrantable.",
         estado: "libre"  // Estado inicial
     },
     {
         nombre: "Sasuke Uchiha",
-        imagen: "https://th.bing.com/th/id/OIP.yY8XGyS5FU5VyJuc-4eDaAHaFj?rs=1&pid=ImgDetMain",
+        imagen: "https://th.bing.com/th/id/R.412d5344e572900055bfbd608acb0731?rik=nQcBTurtsA62CA&riu=http%3a%2f%2fwallsdesk.com%2fwp-content%2fuploads%2f2016%2f12%2fSasuke-Uchiha-full-HD.jpg&ehk=K%2fb2fNix9GjM6OXa3LKArJnFl%2fRBgZ4go9UkODGX6RQ%3d&risl=&pid=ImgRaw&r=0",
         titulo: "El Último Uchiha",
         descripcion: "Un prodigio del clan Uchiha con un pasado sombrío.",
         estado: "libre"  // Estado inicial
     },
+    {
+        nombre: "Hinata",
+        imagen: "https://th.bing.com/th/id/R.522e40edeac3d06ef3c7449ad93166c4?rik=C1FW%2bJ1O%2fb4v1g&riu=http%3a%2f%2fnarutorealm.files.wordpress.com%2f2011%2f11%2fhinata_hyuuga__chapter_437_by_explosivechemicals.png&ehk=Fa3sS0nJkz%2bVrUgVOYX80WlaGmr%2bE6Hm1ddQOhKqYCY%3d&risl=&pid=ImgRaw&r=0",
+        titulo: "La blanquita tetona",
+        descripcion: "nose que decirte, solo vi su rule",
+        estado: "libre"  // Estado inicial
+    },
+    {
+        "nombre": "Goku",
+        "imagen": "https://th.bing.com/th/id/R.3e83b82a418f829fc0375d86e7bbda2b?rik=rplqRTJReHgptA&riu=http%3a%2f%2fvignette3.wikia.nocookie.net%2fdragonball%2fimages%2f3%2f3d%2fGoku.jpg%2frevision%2flatest%3fcb%3d20160725000743&ehk=Hf0SyXtT%2f3%2bi15dzF%2b7TiF98mn%2bi9ziNfDeXB%2bPGE%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Saiyajin Legendario",
+        "descripcion": "Un guerrero de corazón puro y la fuerza de un dios.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Luffy",
+        "imagen": "https://th.bing.com/th/id/R.8d8a4e3d8c4d56f5a3b72e3e0ef5cba6?rik=nI7td5H4vB56Sg&riu=http%3a%2f%2fwww.mangareborn.com%2fwp-content%2fuploads%2f2018%2f12%2fmonkey-d-luffy.jpg&ehk=GJhvK4n2Pbb2cfLZwE3Zf%2bLDF2P8p0pPL2q80X7F8kc%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Capitán de los Sombrero de Paja",
+        "descripcion": "Un aventurero que busca el One Piece y el título de Rey de los Piratas.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Spike Spiegel",
+        "imagen": "https://th.bing.com/th/id/R.f4aa43a69e53688f59d0fd1e0b897fc3?rik=KwL9QuKqzBdXYQ&riu=http%3a%2f%2fcitizencharlie.com%2fwp-content%2fuploads%2f2020%2f03%2fspike-spiegel-1.jpg&ehk=Z2qTRAMobT%2fMG6ztp3vFL27BGTOdT%2fw3r34%2bf7WoqM%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Cazador de Recompensas",
+        "descripcion": "Un cazarrecompensas con un pasado oscuro y un estilo de vida relajado.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Sakura Haruno",
+        "imagen": "https://th.bing.com/th/id/R.69e8927cc2a1d24c82d6d3b831fe1b71?rik=Yx4Jb0FbDgFyFw&riu=http%3a%2f%2fnaruto.fandom.com%2fimages%2f7%2f70%2fSakura_Haruno.png&ehk=LT8FvRGtYFG5zW0ACyz9iT5Z0CDPA0RuqgwnxFGm%2bO0%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "La Ninja Médica",
+        "descripcion": "Una kunoichi que combina habilidades médicas con un gran poder de combate.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Mikasa Ackerman",
+        "imagen": "https://th.bing.com/th/id/R.4d56b0ed7e8eeb074ef68768a6b934fc?rik=n41P3ug4R%2fbt9Q&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f39800000%2fMikasa-Ackerman-attacks-on-titan-39854505-1200-1800.jpg&ehk=e5A9eFyRkXaL8U2cQd2Ba6QJ9j0F6zwAq%2fgEAYYkD0%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "La Guerrera Imparable",
+        "descripcion": "La soldado más fuerte del Cuerpo de Exploración, conocida por su habilidad en combate y lealtad.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Edward Elric",
+        "imagen": "https://th.bing.com/th/id/R.b3d9447994585759d4e0ec1c54e16a7d?rik=91hsFkgWmTkS7g&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fEdward_Elric.png&ehk=Hz3NtoEyHbQsJ3uPx0TY0%2biYkD1dm%2f4sdLhQxhOrF0%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Alquimista de Acero",
+        "descripcion": "Un joven alquimista con un gran talento y un fuerte deseo de restaurar su cuerpo perdido.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Asuka Langley Soryu",
+        "imagen": "https://th.bing.com/th/id/R.59304066b2c62ddbc8307ef7f08e2d2b?rik=Z4h5Efb6Bgs5NA&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fAsuka_Langley_Soryu.png&ehk=n6scMQCrp26JkGrKTwIu2ElcKZKP9vMQhP6J9qQFR1Y%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "La Piloto de EVA",
+        "descripcion": "Una joven piloto de EVA con una personalidad fuerte y habilidades excepcionales en combate.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Inuyasha",
+        "imagen": "https://th.bing.com/th/id/R.5970c9608d84cb7cfc38231bcb4abcf6?rik=UgxHG%2bAKoN4h5w&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fInuyasha.png&ehk=eG%2b0X7snDLRG2Vcs%2bF%2bMYNlKJhV1Z65%2bb6L09Kh1Q%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Híbrido de Demonio y Humano",
+        "descripcion": "Un medio-demonio con una personalidad feroz en busca de la perla de Shikon.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Natsu Dragneel",
+        "imagen": "https://th.bing.com/th/id/R.7f9b530f13c4938dbb295fa98adcb926?rik=fsNNby8xtu7rHQ&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fNatsu_Dragneel.png&ehk=H%2bB2c2Ad5pAeiHQpbnKH5xpr2QBXaJbiVWQdZrK4CGw%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Dragón de Fuego",
+        "descripcion": "Un mago de fuego con una personalidad ardiente y un fuerte sentido de la justicia.",
+        "estado": "libre"
+    },
+    {
+        "nombre": "Light Yagami",
+        "imagen": "https://th.bing.com/th/id/R.79e6c55e66e978d7d3298b98fc7d6d3b?rik=PhOB4A6lbXDBhQ&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fLight_Yagami.png&ehk=F9P%2bW4wTbG8nREapbBrfDmgIfkRTuvIQH4ZMcQUmlJ8%3d&risl=&pid=ImgRaw&r=0",
+        "titulo": "El Rey del Mundo",
+        "descripcion": "Un estudiante brillante que encuentra una libreta con el poder de decidir la vida o muerte de cualquier persona.",
+        "estado": "libre"
+    }
     // Agrega más personajes aquí
 ];
