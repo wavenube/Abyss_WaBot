aaeb7a95d59da9dc5df4a49232257a1a0cb0d721
wavenube <wavenub3@gmail.com>
2024-08-27 17:29:24 +0100

    Update personajes.js

--- plugins/personajes.js
+++ plugins/personajes.js
@@ -5,13 +5,15 @@ export const personajes = [
         nombre: "Naruto Uzumaki",
         imagen: "https://th.bing.com/th/id/R.d536e2ce81f57260a1b086b8eb72cfed?rik=M%2b4DWVkBbtGdHA&pid=ImgRaw&r=0",
         titulo: "El Séptimo Hokage",
-        descripcion: "Un ninja con un gran corazón y una determinación inquebrantable."
+        descripcion: "Un ninja con un gran corazón y una determinación inquebrantable.",
+        estado: "libre"  // Estado inicial
     },
     {
         nombre: "Sasuke Uchiha",
         imagen: "https://th.bing.com/th/id/OIP.yY8XGyS5FU5VyJuc-4eDaAHaFj?rs=1&pid=ImgDetMain",
         titulo: "El Último Uchiha",
-        descripcion: "Un prodigio del clan Uchiha con un pasado sombrío."
+        descripcion: "Un prodigio del clan Uchiha con un pasado sombrío.",
+        estado: "libre"  // Estado inicial
     },
     // Agrega más personajes aquí
 ];
