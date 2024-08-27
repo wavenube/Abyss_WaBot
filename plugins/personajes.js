7b920bb33012cc44f98834f559a6b7cb5aabc114
wavenube <wavenub3@gmail.com>
2024-08-27 17:55:19 +0100

    Update personajes.js

--- plugins/personajes.js
+++ plugins/personajes.js
@@ -24,70 +24,70 @@ export const personajes = [
     },
     {
         "nombre": "Goku",
-        "imagen": "https://th.bing.com/th/id/R.3e83b82a418f829fc0375d86e7bbda2b?rik=rplqRTJReHgptA&riu=http%3a%2f%2fvignette3.wikia.nocookie.net%2fdragonball%2fimages%2f3%2f3d%2fGoku.jpg%2frevision%2flatest%3fcb%3d20160725000743&ehk=Hf0SyXtT%2f3%2bi15dzF%2b7TiF98mn%2bi9ziNfDeXB%2bPGE%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://www.pixelstalk.net/wp-content/uploads/images5/Goku-4K-HD-Wallpaper-Free-download.jpg",
         "titulo": "El Saiyajin Legendario",
         "descripcion": "Un guerrero de corazón puro y la fuerza de un dios.",
         "estado": "libre"
     },
     {
         "nombre": "Luffy",
-        "imagen": "https://th.bing.com/th/id/R.8d8a4e3d8c4d56f5a3b72e3e0ef5cba6?rik=nI7td5H4vB56Sg&riu=http%3a%2f%2fwww.mangareborn.com%2fwp-content%2fuploads%2f2018%2f12%2fmonkey-d-luffy.jpg&ehk=GJhvK4n2Pbb2cfLZwE3Zf%2bLDF2P8p0pPL2q80X7F8kc%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://rare-gallery.com/mocahbig/1375182-luffy-gear-5-vs-kaido-dragon-one-piece-4k-pc.jpg",
         "titulo": "El Capitán de los Sombrero de Paja",
         "descripcion": "Un aventurero que busca el One Piece y el título de Rey de los Piratas.",
         "estado": "libre"
     },
     {
         "nombre": "Spike Spiegel",
-        "imagen": "https://th.bing.com/th/id/R.f4aa43a69e53688f59d0fd1e0b897fc3?rik=KwL9QuKqzBdXYQ&riu=http%3a%2f%2fcitizencharlie.com%2fwp-content%2fuploads%2f2020%2f03%2fspike-spiegel-1.jpg&ehk=Z2qTRAMobT%2fMG6ztp3vFL27BGTOdT%2fw3r34%2bf7WoqM%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://images.wallpapersden.com/image/download/spike-spiegel-cowboy-bebop-4k-art_a25mZ2WUmZqaraWkpJRnamtlrWZpaWU.jpg",
         "titulo": "El Cazador de Recompensas",
         "descripcion": "Un cazarrecompensas con un pasado oscuro y un estilo de vida relajado.",
         "estado": "libre"
     },
     {
         "nombre": "Sakura Haruno",
-        "imagen": "https://th.bing.com/th/id/R.69e8927cc2a1d24c82d6d3b831fe1b71?rik=Yx4Jb0FbDgFyFw&riu=http%3a%2f%2fnaruto.fandom.com%2fimages%2f7%2f70%2fSakura_Haruno.png&ehk=LT8FvRGtYFG5zW0ACyz9iT5Z0CDPA0RuqgwnxFGm%2bO0%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://th.bing.com/th/id/OIP.79eFKkeycKJQM0x6rlwm3AHaEo?rs=1&pid=ImgDetMain",
         "titulo": "La Ninja Médica",
         "descripcion": "Una kunoichi que combina habilidades médicas con un gran poder de combate.",
         "estado": "libre"
     },
     {
         "nombre": "Mikasa Ackerman",
-        "imagen": "https://th.bing.com/th/id/R.4d56b0ed7e8eeb074ef68768a6b934fc?rik=n41P3ug4R%2fbt9Q&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f39800000%2fMikasa-Ackerman-attacks-on-titan-39854505-1200-1800.jpg&ehk=e5A9eFyRkXaL8U2cQd2Ba6QJ9j0F6zwAq%2fgEAYYkD0%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://wallpapercave.com/wp/wp1975719.png",
         "titulo": "La Guerrera Imparable",
         "descripcion": "La soldado más fuerte del Cuerpo de Exploración, conocida por su habilidad en combate y lealtad.",
         "estado": "libre"
     },
     {
         "nombre": "Edward Elric",
-        "imagen": "https://th.bing.com/th/id/R.b3d9447994585759d4e0ec1c54e16a7d?rik=91hsFkgWmTkS7g&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fEdward_Elric.png&ehk=Hz3NtoEyHbQsJ3uPx0TY0%2biYkD1dm%2f4sdLhQxhOrF0%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://th.bing.com/th/id/R.3fe0270b01778ca48adc11ff6c28ff86?rik=Y2viXirUXqrx4w&pid=ImgRaw&r=0",
         "titulo": "El Alquimista de Acero",
         "descripcion": "Un joven alquimista con un gran talento y un fuerte deseo de restaurar su cuerpo perdido.",
         "estado": "libre"
     },
     {
         "nombre": "Asuka Langley Soryu",
-        "imagen": "https://th.bing.com/th/id/R.59304066b2c62ddbc8307ef7f08e2d2b?rik=Z4h5Efb6Bgs5NA&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fAsuka_Langley_Soryu.png&ehk=n6scMQCrp26JkGrKTwIu2ElcKZKP9vMQhP6J9qQFR1Y%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://wallpaperaccess.com/full/1977407.png",
         "titulo": "La Piloto de EVA",
         "descripcion": "Una joven piloto de EVA con una personalidad fuerte y habilidades excepcionales en combate.",
         "estado": "libre"
     },
     {
         "nombre": "Inuyasha",
-        "imagen": "https://th.bing.com/th/id/R.5970c9608d84cb7cfc38231bcb4abcf6?rik=UgxHG%2bAKoN4h5w&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fInuyasha.png&ehk=eG%2b0X7snDLRG2Vcs%2bF%2bMYNlKJhV1Z65%2bb6L09Kh1Q%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://th.bing.com/th/id/R.af80b26c6bd5a71eba219eb1e10b579b?rik=sPxSusXKl81zDg&pid=ImgRaw&r=0",
         "titulo": "El Híbrido de Demonio y Humano",
         "descripcion": "Un medio-demonio con una personalidad feroz en busca de la perla de Shikon.",
         "estado": "libre"
     },
     {
         "nombre": "Natsu Dragneel",
-        "imagen": "https://th.bing.com/th/id/R.7f9b530f13c4938dbb295fa98adcb926?rik=fsNNby8xtu7rHQ&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fNatsu_Dragneel.png&ehk=H%2bB2c2Ad5pAeiHQpbnKH5xpr2QBXaJbiVWQdZrK4CGw%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://th.bing.com/th/id/R.dcd8ee87895b6a350efcc357a5cf1758?rik=xxowEpoYfol%2bDQ&pid=ImgRaw&r=0",
         "titulo": "El Dragón de Fuego",
         "descripcion": "Un mago de fuego con una personalidad ardiente y un fuerte sentido de la justicia.",
         "estado": "libre"
     },
     {
         "nombre": "Light Yagami",
-        "imagen": "https://th.bing.com/th/id/R.79e6c55e66e978d7d3298b98fc7d6d3b?rik=PhOB4A6lbXDBhQ&riu=http%3a%2f%2fwww.animecharactersdatabase.com%2fcharacters%2fimages%2fLight_Yagami.png&ehk=F9P%2bW4wTbG8nREapbBrfDmgIfkRTuvIQH4ZMcQUmlJ8%3d&risl=&pid=ImgRaw&r=0",
+        "imagen": "https://getwallpapers.com/wallpaper/full/2/e/0/1386406-light-yagami-wallpaper-1920x1080-for-iphone-5s.jpg",
         "titulo": "El Rey del Mundo",
         "descripcion": "Un estudiante brillante que encuentra una libreta con el poder de decidir la vida o muerte de cualquier persona.",
         "estado": "libre"
