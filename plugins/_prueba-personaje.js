60208b0b781ff6364f5b0ac6cd28ac443481c437
wavenube <wavenub3@gmail.com>
2024-08-27 16:27:02 +0100

    Create _prueba-personaje.js

---
+++ plugins/_prueba-personaje.js
@@ -0,0 +1,16 @@
+// En un archivo de configuración o en la parte superior del script
+const personajes = [
+    {
+        nombre: "Naruto Uzumaki",
+        imagen: "https://th.bing.com/th/id/R.d536e2ce81f57260a1b086b8eb72cfed?rik=M%2b4DWVkBbtGdHA&pid=ImgRaw&r=0",
+        titulo: "El Séptimo Hokage",
+        descripcion: "Un ninja con un gran corazón y una determinación inquebrantable."
+    },
+    {
+        nombre: "Sasuke Uchiha",
+        imagen: "https://th.bing.com/th/id/OIP.yY8XGyS5FU5VyJuc-4eDaAHaFj?rs=1&pid=ImgDetMain",
+        titulo: "El Último Uchiha",
+        descripcion: "Un prodigio del clan Uchiha con un pasado sombrío."
+    },
+    // Agrega más personajes aquí
+];
