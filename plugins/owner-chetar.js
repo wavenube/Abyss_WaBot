1ea4576c5d0ed5c3476360e82264f8ffc9af1d81
wavenube <wavenub3@gmail.com>
2024-08-28 18:54:23 +0100

    Update owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -9,10 +9,10 @@ const handler = async (m, { conn }) => {
 
     const user = global.db.data.users[m.sender];
         conn.sendMessage(m.chat, {text: `*[❗] @${m.sender.split('@')[0]} ${tradutor.texto1}`, mentions: [m.sender]}, {quoted: m});
-      global.db.data.users[m.sender].money = Infinity;
-    global.db.data.users[m.sender].limit = Infinity;
-  global.db.data.users[m.sender].level = Infinity;
- global.db.data.users[m.sender].exp = Infinity;
+      global.db.data.users[m.sender].money = 10000000;
+    global.db.data.users[m.sender].limit = 10000000;
+  global.db.data.users[m.sender].level = 10000000;
+ global.db.data.users[m.sender].exp = 10000000;
 };
 handler.help = ['cheat'];
 handler.tags = ['owner'];
