a63aa4b91003ba645a309707c5dddf376bf9a86f
wavenube <wavenub3@gmail.com>
2024-08-28 12:14:23 +0100

    Update rpg-pujarch.js

--- plugins/rpg-pujarch.js
+++ plugins/rpg-pujarch.js
@@ -3,7 +3,8 @@ import { personajes } from './personajes.js';
 // Definir pujaData globalmente en el archivo
 global.pujaData = {
     personajes: [],
-    finPuja: null
+    finPuja: null,
+    pujadores: {} // Nuevo: Para rastrear quién ha pujado
 };
 
 const handlerPuja = async (m, { conn }) => {
@@ -18,6 +19,7 @@ const handlerPuja = async (m, { conn }) => {
     // Selecciona 3 personajes al azar
     global.pujaData.personajes = personajes.sort(() => 0.5 - Math.random()).slice(0, 3);
     global.pujaData.finPuja = ahora + 3600000; // 1 hora en milisegundos
+    global.pujaData.pujadores = {}; // Restablecer los pujadores
 
     let mensaje = `🔔 *¡Puja Iniciada!*\n\n`;
 
@@ -28,6 +30,19 @@ const handlerPuja = async (m, { conn }) => {
     mensaje += `🕒 La puja finalizará en 1 hora. Usa .pujar <nombre del personaje> para pujar.`;
 
     await conn.sendMessage(m.chat, { text: mensaje }, { quoted: m });
+
+    // Restaurar valores originales después de 1 hora
+    setTimeout(() => {
+        global.pujaData.personajes.forEach(p => {
+            const personajeOriginal = personajes.find(personaje => personaje.nombre === p.nombre);
+            if (personajeOriginal) {
+                p.valor = personajeOriginal.valor;
+            }
+        });
+        global.pujaData.personajes = [];
+        global.pujaData.finPuja = null;
+        global.pujaData.pujadores = {};
+    }, 3600000); // 1 hora en milisegundos
 };
 
 // Configuración del comando
