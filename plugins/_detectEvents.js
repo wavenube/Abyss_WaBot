cef039e41344c93d5de2de032bb95b8131897495
wavenube <wavenub3@gmail.com>
2024-08-30 13:10:16 +0100

    Update _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -4,7 +4,7 @@
 
 /* Bot: https://github.com/Gatito-kw/nekobot-md */
 
-import {WAMessageStubType} from '@whiskeysockets/baileys';
+import {WAMessageStubType} from "baileys";
 import fetch from 'node-fetch';
 
  // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
@@ -14,7 +14,7 @@ import fetch from 'node-fetch';
 export async function before(m, {conn, participants}) {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins._detectevents
 
   if (!m.messageStubType || !m.isGroup) return !0;
